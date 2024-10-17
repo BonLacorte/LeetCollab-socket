147f6736684a74ee9e73475669b019cd580245a0
Bon Lacorte <bonbonlacorte123@gmail.com>
2024-10-18 01:27:33 +0800

    postcss fix

--- index.ts
+++ index.ts
@@ -14,7 +14,8 @@ const app = express();
 const server = http.createServer(app); // Create HTTP server
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        // origin: 'http://localhost:3000',
+        origin: 'https://leetcollab.vercel.app/',
         credentials: true,
         methods: ['GET', 'POST'],
     },
